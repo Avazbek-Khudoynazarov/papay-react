45494d3e4d88d21e6aa7492f183c2de450b31fea
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-02-22 20:59:09 +0900

    feat: Footer and Homepage part one

---
+++ src/app/screens/HomePage/advertisements.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/system";
+import React from "react";
+
+export function Advertisements() {
+  return <div className="top_restaurant_frame">Advertisements</div>;
+}
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,6 +1,25 @@
+import { Stack } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
+import { Advertisements } from "./advertisements";
+import { BestDishes } from "./bestDishes";
+import { BestRestaurants } from "./bestRestaurants ";
+import { Events } from "./events";
+import { Recommendations } from "./recommendations";
+import { Statistics } from "./statistics";
+import { TopRestaurants } from "./topRestaurants";
+import "../../../css/home.css";
 
 export function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className="homepage">
+      <Statistics />
+      <TopRestaurants />
+      <BestRestaurants />
+      <BestDishes />
+      <Advertisements />
+      <Events />
+      <Recommendations />
+    </div>
+  );
 }
